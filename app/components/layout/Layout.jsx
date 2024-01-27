cc69e48e97acfeb78cd4f4a0521315a1195dfeff
Javlon1 <muhamedmijotot@gmail.com>
2024-01-27 14:24:51 +0500

    manifest v-2

--- app/components/layout/Layout.jsx
+++ app/components/layout/Layout.jsx
@@ -17,4 +17,14 @@ const Layout = ({ children }) => {
     )
 }
 
+export const metaData = {
+    name: "pyblog",
+    short_name: "pyblog",
+    description: "pyblog bu Python va dasturlashga oid o’zbek tilidagi blog. Hozirda Python, Javascript, Dasturlash, Veb dasturlash, Foundation va boshqa turkumlarga va o’nlab teglarga mansub maqolalar saytdan joy olgan.",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#D3EFEA",
+    theme_color: "#0D1212",
+    manifest: "/site.webmanifest"
+}
 export default Layout;
