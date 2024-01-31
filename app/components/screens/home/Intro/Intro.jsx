611b94222e404ee8f5fa52b37fc8b2d244560269
Javlon1 <muhamedmijotot@gmail.com>
2024-01-31 11:47:23 +0500

    accessibility desktop btn

--- app/components/screens/home/Intro/Intro.jsx
+++ app/components/screens/home/Intro/Intro.jsx
@@ -238,19 +238,21 @@ const Intro = () => {
                                         setTagAct(!tagAct)
                                     }}
                                 >
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        viewBox="0 0 24 24"
-                                        width={30}
-                                        height={30}
-                                        fill="none"
-                                        stroke="currentColor"
-                                        strokeWidth="2"
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                    >
-                                        <path d="M6 9l6 6 6-6" />
-                                    </svg>
+                                    <span id="button-label">
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 24 24"
+                                            width={30}
+                                            height={30}
+                                            fill="none"
+                                            stroke="currentColor"
+                                            strokeWidth="2"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                        >
+                                            <path d="M6 9l6 6 6-6" />
+                                        </svg>
+                                    </span>
                                 </button>
                             </ul>
                         </div>
